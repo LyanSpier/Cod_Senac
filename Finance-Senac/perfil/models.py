1bb8ce04abf8efc0510bc924bb0e727dfe8235db
Raylan <raylan_aviz2@hotmail.com>
2023-08-04 20:48:32 -0300

    update

--- Finance-Senac/perfil/models.py
+++ Finance-Senac/perfil/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class categoria(models.Model):
+    categoria = models.CharField(max_length=50,null=False,blank=False, verbose_name='categoria')
+    essencial = models.BooleanField(default=False)
+    valor_planejamento = models.FloatField(default=0, verbose_name='valor do planejamento')
+    
+    def __str__(self):
+        return self.categoria
+    
+class conta(models.Model):
+    banco_choices = (
+        ('NU', "nubank"),
+        ('CX', "Caixa"),
+    )
+    
+    tipo_choices = (
+        ('pf','Pessoa Física'),
+        ('pJ','Pessoa Jurídica')
+    )
+    
+    apelido = models.CharField(max_length=50)
+    banco = models.CharField(max_length=2, choices=banco_choices)
+    tipo = models.CharField(max_length=2,choices=tipo_choices)
+    valor = models.FloatField()
+    icone = models.ImageField(upload_to='Icone')
+    
+    def __str__(self):
+        return self.apelido
+    
